095c5e20b5deff0c3648a92dae0c47fef4facb11
nerevand <kruchkovskiy2@gmail.com>
2020-10-25 15:26:04 +0200

    Updates

---
+++ precache-manifest.ebf6e8de3e81ace1de5523962051b450.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2267f2a152177567f922026d3ba96cf",
+    "url": "/calendar/index.html"
+  },
+  {
+    "revision": "5e27a97c678a8cfa2a17",
+    "url": "/calendar/static/css/main.080bd3e2.chunk.css"
+  },
+  {
+    "revision": "56b828cd4a6b25890d63",
+    "url": "/calendar/static/js/2.a264a5b8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/calendar/static/js/2.a264a5b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e27a97c678a8cfa2a17",
+    "url": "/calendar/static/js/main.3efd7a00.chunk.js"
+  },
+  {
+    "revision": "1156b047107387650300",
+    "url": "/calendar/static/js/runtime-main.f5e793c0.js"
+  }
+]);
